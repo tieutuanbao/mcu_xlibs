4695e7168c51fe403be979d5082a7bf4ef4e6941
tieutuanbao <tieutuanbao@gmail.com>
2022-08-25 18:46:21 +0700

    update lib string

--- misc/bits_string.c
+++ misc/bits_string.c
@@ -1,7 +1,7 @@
 #include "bits_string.h"
 
 
-void string_to_lower(char *c) {
+void Bits_String_ToLower(char *c) {
     if(c == 0) return;
     while(*c != 0){
         if((*c >= 'A') || (*c <= 'Z')) *c += 'a' - 'A';
@@ -9,7 +9,22 @@ void string_to_lower(char *c) {
     }
 }
 
-char char_to_lower(char c) {
+char Bits_Char_ToLower(char c) {
     if((c >= 'A') || (c <= 'Z')) c += 'a' - 'A';
     else return c;
+}
+
+long int Bits_String_ToLongInt(char *str) {
+    long int ret = 0;
+    uint8_t idx_digit = 0;
+    for(idx_digit = 0; idx_digit < 10; idx_digit++) {
+        if(Bits_CharIsNum(*str)) {
+            ret = (ret * 10) + Bits_CharToNum(*str);
+        }
+        else {
+            return ret;
+        }
+        str++;
+    }
+    return ret;
 }
--- misc/bits_string.h
+++ misc/bits_string.h
@@ -1,8 +1,17 @@
 #ifndef __BITS_STRING_H
 #define __BITS_STRING_H
 
+#ifndef bool
+#include <stdbool.h>
+#endif
 
-void string_to_lower(char *c);
-char char_to_lower(char c);
+#include <stdint.h>
+
+#define Bits_CharIsNum(n)       (((n) >= '0') && ((n) <= '9'))
+#define Bits_CharToNum(n)       (n - '0')
+
+void Bits_String_ToLower(char *c);
+char Bits_Char_ToLower(char c);
+long int Bits_String_ToLongInt(char *str);
 
 #endif
