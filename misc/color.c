c484f54c2f19cd1f6089f5d6c09d6ea26035d198
tieutuanbao <tieutuanbao@gmail.com>
2022-10-31 00:28:32 +0700

    change structure color

--- misc/color.c
+++ misc/color.c
@@ -1,7 +1,6 @@
 #include "color.h"
 
-
-const rgb_color_t sample_color[COLOR_IDX_MAX] = {
+const Color_RGB_t SampleColor[] = {
 	{0x00,0x00,0x00},
 	{0xff,0x00,0x00},
 	{0xff,0x80,0x00},
--- misc/color.h
+++ misc/color.h
@@ -3,31 +3,32 @@
 
 #include <stdint.h>
 
+typedef struct __attribute__((packed)) {
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+} Color_RGB_t;
 
 typedef struct __attribute__((packed)) {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} rgb_color_t;
+    Color_RGB_t RGB;
+    uint8_t A;
+} Color_ARGB_t;
 
-typedef enum {
-	COLOR_BLACK,
-	COLOR_RED,
-	COLOR_ORANGE,
-	COLOR_YELLOW,
-	COLOR_CHART_GREEN,
-	COLOR_GREEN,
-	COLOR_SPRING_GREEN,
-	COLOR_CYAN,
-	COLOR_AZURE,
-	COLOR_BLUE,
-	COLOR_VIOLET,
-	COLOR_MAGENTA,
-	COLOR_ROSE,
-	COLOR_WHITE,
-    COLOR_IDX_MAX
-} rgb_color_sample_t;
+#define COLOR_BLACK	            SampleColor[0]
+#define COLOR_RED               SampleColor[1]
+#define COLOR_ORANGE            SampleColor[2]
+#define COLOR_YELLOW            SampleColor[3]
+#define COLOR_CHART_GREEN       SampleColor[4]
+#define COLOR_GREEN             SampleColor[5]
+#define COLOR_SPRING_GREEN      SampleColor[6]
+#define COLOR_CYAN              SampleColor[7]
+#define COLOR_AZURE             SampleColor[8]
+#define COLOR_BLUE              SampleColor[9]
+#define COLOR_VIOLET            SampleColor[10]
+#define COLOR_MAGENTA           SampleColor[11]
+#define COLOR_ROSE              SampleColor[12]
+#define COLOR_WHITE             SampleColor[13]
 
-extern const rgb_color_t sample_color[];
+extern const Color_RGB_t SampleColor[];
 
 #endif
