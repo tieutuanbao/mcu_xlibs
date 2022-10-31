6213f9cc7dd31ddce4cac6ce319116a167730a1b
tieutuanbao <tieutuanbao@gmail.com>
2022-10-31 18:22:39 +0700

    repair 32bit data sound

--- components/audio/audio_output_i2s_noDAC.c
+++ components/audio/audio_output_i2s_noDAC.c
@@ -18,6 +18,7 @@ audio_output_stt_t i2s_drv_init(i2s_no_dac_t *dev, uint8_t num_channel, uint32_t
         .data_out_en = 1,
         .data_in_en = 0
     };
+    i2s_no_dac_setGain(dev, 1);
     i2s_dma_init(0, &dev->i2s_config, &pin_config);
 }
 /**
@@ -41,72 +42,48 @@ void i2s_no_dac_stop(i2s_no_dac_t *dev){
     i2s_dma_stop(0);
 }
 
-int16_t i2s_no_dac_amplify(int32_t s) {
-    int32_t v = (s * 64) >> 6;
-    if (v < -32767) return -32767;
-    else if (v > 32767) return 32767;
-    else return (int16_t)(v & 0xffff);
+void i2s_no_dac_setGain(i2s_no_dac_t *dev, float fgain) {
+    dev->gain = fgain * 64;
 }
-
 static inline void i2s_no_dac_deltasigma(i2s_no_dac_t *dev, int32_t *sample, uint32_t *delta_buff, uint16_t num_sample) {
-    int32_t sum;
-    int32_t new_sample;
-    int32_t diffPerStep;
-    uint32_t bits = 0;
+    int32_t new_sample = 0;
+    int32_t diffPerStep = 0;
     for(uint16_t index_sample = 0; index_sample < num_sample; index_sample++) {
         // BITS_LOG("Sample %d: %08X -- %08X, ", dev->index_sample, sample[index_sample * 2], sample[index_sample * 2 + 1]);
         /* Tính trung bình 2 giá trị LR */
-        sum = (((int32_t)sample[index_sample * 2]) + ((int32_t)sample[index_sample * 2 + 1])) >> 1;
-        // printf("sum: %08X ; ", sum);
-        new_sample = ( (int32_t)i2s_no_dac_amplify(sum) ) << 8;
+        new_sample = ((sample[index_sample * 2]) + (sample[index_sample * 2 + 1])) >> 1;
+        new_sample *= dev->gain;
+        new_sample <<= 2;
         // printf("new %08X, ", new_sample);
         // How much the comparison signal changes each oversample step
         diffPerStep = (new_sample - dev->last_sample) >> 5;
         // Don't need last_sample anymore, store this one for next round
         dev->last_sample = new_sample;
-        bits = 0; // The bits we convert the sample into, MSB to go on the wire first
-        for (uint8_t index_bit = 32; index_bit > 0; index_bit--) {
-            bits = bits << 1;
+        delta_buff[index_sample] = 0; // The bits we convert the sample into, MSB to go on the wire first
+        for (uint8_t index_bit = 0; index_bit < 32; index_bit++) {
+            delta_buff[index_sample] = delta_buff[index_sample] << 1;
             if (dev->cum_error < 0) {
-                bits |= 1;
-                dev->cum_error += 0x007fff00 - new_sample;
+                delta_buff[index_sample] |= 1ULL;
+                dev->cum_error += 0x007FFF00 - new_sample;
             } else {
                 // Bits[0] = 0 handled already by left shift
-                dev->cum_error -= 0x007fff00 + new_sample;
+                dev->cum_error -= 0x007FFF00 + new_sample;
             }
             new_sample += diffPerStep; // Move the reference signal towards destination
         }
-        delta_buff[index_sample] = bits;
         // printf("dsbuff: %x\n", delta_buff[index_sample]);
-        dev->index_sample++;
-    }
-}
-
-static inline void i2s_make_sample_stereo32(i2s_no_dac_t *dev, int32_t *sample_r,  int32_t *sample_l){
-    if(dev->i2s_config.bits_per_sample == 8) {
-        sample_r[0] = ((sample_r[0] & 0xFF) - 128) << 8 ;
-        sample_l[0] = ((sample_l[0] & 0xFF) - 128) << 8 ;
+        // dev->index_sample++;
     }
 }
 
 audio_output_stt_t i2s_no_dac_consume_sample(i2s_no_dac_t *dev, int32_t *sample, uint16_t num_sample) {
     uint32_t delta_buff[num_sample];
-    int32_t temp_sample[num_sample * 2];
-
     if((sample == 0) || (num_sample == 0)) {
         BITS_LOGE("Sample buffer error!!\r\n");
         return audio_output_sample_err;
     }
-    for(uint16_t index_sample = 0; index_sample < num_sample; index_sample++) {
-        temp_sample[index_sample * 2] = sample[index_sample * 2];
-        temp_sample[index_sample * 2 + 1] = sample[index_sample * 2 + 1];
-        // i2s_make_sample_stereo32(dev, &temp_sample[index_sample * 2], &temp_sample[index_sample * 2 + 1]);
-    }
-
-    i2s_no_dac_deltasigma(dev, temp_sample, delta_buff, num_sample);
-
+    i2s_no_dac_deltasigma(dev, sample, delta_buff, num_sample);
     i2s_write_buffer((int16_t *)delta_buff, num_sample);
-    // BITS_LOG("I2s Byte Writen: %d!!\r\n", dev->byte_written);
     return audio_output_ok;
 }
 
--- components/audio/audio_output_i2s_noDAC.h
+++ components/audio/audio_output_i2s_noDAC.h
@@ -11,13 +11,13 @@ typedef struct{
 
     i2s_config_t i2s_config;
 
-    int32_t last_sample;  // Last sample value
-    int32_t cum_error;    // Running cumulative error since time began
-    uint8_t oversample;
-    uint16_t hertz;
+    int32_t last_sample;        // Last sample value
+    int32_t cum_error;          // Running cumulative error since time began
     size_t byte_written;
     uint32_t index_sample;
+    uint8_t gain;
 } i2s_no_dac_t;
 
+void i2s_no_dac_setGain(i2s_no_dac_t *dev, float fgain);
 
 #endif
