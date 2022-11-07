496d7876e051677527d09c8db837dcfe2653d9d1
tieutuanbao <tieutuanbao@gmail.com>
2022-11-07 12:57:23 +0700

    ds1307 add new function

--- components/ds1307/ds1307.c
+++ components/ds1307/ds1307.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file           : ds1307.c
   * @brief          : Thư viện lập trình ds1307.
-    *    @author                    :    Tiêu Tuấn Bảo
+  * @author         : Tiêu Tuấn Bảo
   ******************************************************************************
   * @attention
   *
@@ -21,83 +21,98 @@ static uint8_t ds1307_encode(uint8_t dec) {
 }
 
 
-uint8_t ds1307_init(ds1307_t *dev, uint8_t (*read)(uint8_t addr, uint8_t reg, uint8_t *val), uint8_t (*write)(uint8_t addr, uint8_t reg, uint8_t val)) {
+bool ds1307_init(ds1307_t *dev, uint8_t (*read)(uint8_t addr, uint8_t reg, uint8_t *Data, uint8_t DataLength), uint8_t (*write)(uint8_t addr, uint8_t reg, uint8_t *Data, uint8_t DataLength)) {
     dev->read = read;
     dev->write = write;
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_CONTROL, 0x00)) return 1;
-    return 0;
+    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_CONTROL, (uint8_t []){0x00}, 1)) return false;
+    return true;
 }
 
-uint8_t ds1307_get_clock(ds1307_t *dev) {
-    uint8_t temp_val = 0;
-    /* Lấy dữ liệu giây */
-    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_SECONDS, &temp_val)) return 1;
-    dev->time.seconds = ds1307_decode(temp_val);
-    /* Lấy dữ liệu phút */
-    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_MINUTE, &temp_val)) return 1;
-    dev->time.minute = ds1307_decode(temp_val);
-    /* Lấy dữ liệu giờ */
-    switch(dev->time.format) {
-        case ds1307_time_format_12h:
-            if(dev->read(DS1307_I2C_ADDR, DS1307_REG_HOUR, &temp_val)) return 1;
-            dev->time.am_or_pm = (ds1307_hour_aop_t)(temp_val >> 5);
-            dev->time.hour = ds1307_decode(0x1F & temp_val);
-            break;
-        default:
-            if(dev->read(DS1307_I2C_ADDR, DS1307_REG_HOUR, &temp_val)) return 1;
-            dev->time.hour = ds1307_decode(0x3F & temp_val);
-            break;
+bool ds1307_get_clock(ds1307_t *dev) {
+    uint8_t temp_val[3];
+    /* Lấy dữ liệu */
+    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_SECONDS, temp_val, 3)) return false;
+    /* Decode dữ liệu giây */
+    dev->time.seconds = ds1307_decode(temp_val[0]);
+    /* Decode dữ liệu phút */
+    dev->time.minute = ds1307_decode(temp_val[1]);
+    /* Decode dữ liệu giờ */
+    if(temp_val[2] & 0x40) {
+        dev->time.format = ds1307_time_format_12h;
+        dev->time.am_or_pm = (ds1307_hour_aop_t)(temp_val[2] >> 5);
+        dev->time.hour = ds1307_decode(0x1F & temp_val[2]);
     }
-    return 0;
+    else {
+        dev->time.format = ds1307_time_format_24h;
+        dev->time.hour = ds1307_decode(0x3F & temp_val[2]);
+    }
+    return true;
 }
 
-uint8_t ds1307_get_calendar(ds1307_t *dev) {
-    uint8_t temp_val = 0;
-    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_YEAR, &temp_val)) return 1;
-    dev->time.year = ds1307_decode(temp_val);
-    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_MONTH, &temp_val)) return 1;
-    temp_val = (0x1F & temp_val);
-    dev->time.month = ds1307_decode(temp_val);
-    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_DATE, &temp_val)) return 1;
-    temp_val = (0x3F & temp_val);
-    dev->time.date = ds1307_decode(temp_val);
-    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_DAY, &temp_val)) return 1;
-    temp_val = (0x07 & temp_val);
-    dev->time.day = ds1307_decode(temp_val);
-    return 0;
+bool ds1307_get_calendar(ds1307_t *dev) {
+    uint8_t temp_val[4];
+    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_DAY, temp_val, 4)) return false;
+    temp_val[0] = (0x07 & temp_val[0]);
+    dev->time.day = ds1307_decode(temp_val[0]);
+    temp_val[1] = (0x3F & temp_val[1]);
+    dev->time.date = ds1307_decode(temp_val[1]);
+    temp_val[2] = (0x1F & temp_val[2]);
+    dev->time.month = ds1307_decode(temp_val[2]);
+    dev->time.year = ds1307_decode(temp_val[3]);
+    return true;
 }
 
-uint8_t ds1307_set_clock(ds1307_t *dev, uint8_t hour, uint8_t minute, uint8_t seconds, ds1307_hour_aop_t am_or_pm, ds1307_time_format_t format) {
-    uint8_t temp_val = 0;
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_SECONDS, ds1307_encode(seconds))) return 1;
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_MINUTE, ds1307_encode(minute))) return 1;
+bool ds1307_set_clock(ds1307_t *dev, uint8_t hour, uint8_t minute, uint8_t seconds, ds1307_hour_aop_t am_or_pm, ds1307_time_format_t format) {
+    uint8_t temp_val[3];
+    temp_val[0] = ds1307_encode(seconds);
+    temp_val[1] = ds1307_encode(minute);
     switch(format) {
         case ds1307_time_format_12h: {
             switch(am_or_pm) {
                 case ds1307_hour_pm: {
-                    temp_val = 0x60;
+                    temp_val[2] = 0x60;
                     break;
                 }
                 default: {
-                    temp_val = 0x40;
+                    temp_val[2] = 0x40;
                     break;
                 }
             }
-            if(dev->write(DS1307_I2C_ADDR, DS1307_REG_HOUR, ((temp_val | (0x1F & (ds1307_encode(hour))))))) return 1;
+            temp_val[2] |= (0x1F & (ds1307_encode(hour)));
             break;
         }
         default: {
-            if(dev->write(DS1307_I2C_ADDR, DS1307_REG_HOUR, (0x3F & (ds1307_encode(hour))))) return 1;
+            temp_val[2] = (0x3F & (ds1307_encode(hour)));
             break;
         }
     }
-    return 0;
+    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_SECONDS, temp_val, 3)) return false;
+    return true;
 }
 
-uint8_t ds1307_set_calendar(ds1307_t *dev, uint8_t day, uint8_t date, uint8_t month, uint8_t year) {
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_DAY, ds1307_encode(day))) return 1;
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_DATE, ds1307_encode(date))) return 1;
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_MONTH, ds1307_encode(month))) return 1;
-    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_YEAR, ds1307_encode(year))) return 1;
-    return 0;
+bool ds1307_set_calendar(ds1307_t *dev, uint8_t day, uint8_t date, uint8_t month, uint8_t year) {
+    uint8_t temp_val[4];
+    temp_val[0] = ds1307_encode(day);
+    temp_val[1] = ds1307_encode(date);
+    temp_val[2] = ds1307_encode(month);
+    temp_val[3] = ds1307_encode(year);
+    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_DAY, temp_val, 4)) return false;
+    return true;
+}
+
+bool ds1307_set_SQW(ds1307_t *dev, ds1307_sqw_t sqw_mode) {
+    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_CONTROL, (uint8_t []){0x10}, 1)) return false;
+    return true;
+}
+
+bool ds1307_write_Ram(ds1307_t *dev, uint8_t *Offset, uint8_t *Data, uint8_t DataLength) {
+    if(DataLength == 0) return false;
+    if(dev->write(DS1307_I2C_ADDR, DS1307_REG_RAM, Data, DataLength)) return false;
+    return true;
+}
+
+bool ds1307_read_Ram(ds1307_t *dev, uint8_t *Offset, uint8_t *Data, uint8_t DataLength) {
+    if(DataLength == 0) return false;
+    if(dev->read(DS1307_I2C_ADDR, DS1307_REG_RAM, Data, DataLength)) return false;
+    return true;
 }
--- components/ds1307/ds1307.h
+++ components/ds1307/ds1307.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file           : ds1307.h
   * @brief          : Thư viện lập trình ds1307.
-    *    @author                    :    Tiêu Tuấn Bảo
+  * @author         :    Tiêu Tuấn Bảo
   ******************************************************************************
   * @attention
   *
@@ -12,6 +12,7 @@
 #define __DS1307_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 /* Địa chỉ I2C giao tiếp DS1307 */
 #define DS1307_I2C_ADDR         0xD0
@@ -37,6 +38,15 @@ typedef enum {
     ds1307_time_format_12h  = 1
 } ds1307_time_format_t;
 
+typedef enum {
+    ds1307_sqw_OUT0         = 0x00,
+    ds1307_sqw_OUT1         = 0xF0,
+    ds1307_sqw_1Hz          = 0x10,
+    ds1307_sqw_4096Hz       = 0x11,
+    ds1307_sqw_8192Hz       = 0x12,
+    ds1307_sqw_32768Hz      = 0x13
+} ds1307_sqw_t;
+
 typedef struct {
     uint8_t hour;
     uint8_t minute;
@@ -47,20 +57,23 @@ typedef struct {
     uint8_t year;
     ds1307_hour_aop_t am_or_pm;
     ds1307_time_format_t format;
-}    ds1307_time_t;
+} ds1307_time_t;
 
 typedef struct {
-    uint8_t (*read)(uint8_t addr, uint8_t reg, uint8_t *val);
-    uint8_t (*write)(uint8_t addr, uint8_t reg, uint8_t val);
+    uint8_t (*read)(uint8_t addr, uint8_t reg, uint8_t *Dat, uint8_t DatLength);
+    uint8_t (*write)(uint8_t addr, uint8_t reg, uint8_t *Dat, uint8_t DatLength);
 
     ds1307_time_t time;
 } ds1307_t;
 
-uint8_t ds1307_init(ds1307_t *dev, uint8_t (*read)(uint8_t addr, uint8_t reg, uint8_t *val), uint8_t (*write)(uint8_t addr, uint8_t reg, uint8_t val));
+bool ds1307_init(ds1307_t *dev, uint8_t (*read)(uint8_t addr, uint8_t reg, uint8_t *Data, uint8_t DataLength), uint8_t (*write)(uint8_t addr, uint8_t reg, uint8_t *Data, uint8_t DataLength));
 
-uint8_t ds1307_get_clock(ds1307_t *dev);
-uint8_t ds1307_get_calendar(ds1307_t *dev);
-uint8_t ds1307_set_clock(ds1307_t *dev, uint8_t hour, uint8_t minute, uint8_t seconds, ds1307_hour_aop_t am_or_pm, ds1307_time_format_t format);
-uint8_t ds1307_set_calendar(ds1307_t *dev, uint8_t day, uint8_t date, uint8_t month, uint8_t year);
+bool ds1307_get_clock(ds1307_t *dev);
+bool ds1307_get_calendar(ds1307_t *dev);
+bool ds1307_set_clock(ds1307_t *dev, uint8_t hour, uint8_t minute, uint8_t seconds, ds1307_hour_aop_t am_or_pm, ds1307_time_format_t format);
+bool ds1307_set_calendar(ds1307_t *dev, uint8_t day, uint8_t date, uint8_t month, uint8_t year);
+bool ds1307_set_SQW(ds1307_t *dev, ds1307_sqw_t sqw_mode);
+bool ds1307_write_Ram(ds1307_t *dev, uint8_t *Offset, uint8_t *Data, uint8_t DataLength);
+bool ds1307_read_Ram(ds1307_t *dev, uint8_t *Offset, uint8_t *Data, uint8_t DataLength);
 
 #endif
