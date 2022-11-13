967de6f98c6901e37d0c34beb2b0d9c2457e09de
tieutuanbao <tieutuanbao@gmail.com>
2022-11-13 17:37:43 +0700

    add auto align center

--- components/EZGUI/source/ezgui_label.c
+++ components/EZGUI/source/ezgui_label.c
@@ -5,7 +5,24 @@
 
 static void Label_Draw(DrawPoint_t DrawPoint_Drv, EZGUI_Label_t *Label, Graphics_t *Graphic, Point_t Offset) {
     if(Label->Text) {
-        EZGUI_Draw_String(DrawPoint_Drv, Graphic, (Point_t){Label->Object.Point.X + Offset.X, Label->Object.Point.Y + Offset.Y}, Label->Text, (void *)&Label->Color.ForeGround, Label->Font);
+        Offset = (Point_t){Label->Object.Point.X + Offset.X, Label->Object.Point.Y + Offset.Y};
+        Font_Decode_t CharInfo;
+        Size_t TotalString = {0, 0};
+        /* Lấy tổng chiều cao và tổng chiều ngang */
+        for(uint16_t IndexChar = 0; Label->Text[IndexChar] != 0x00; IndexChar += CharInfo.ByteCount) {
+            CharInfo = Font_DecodeChar(Label->Text + IndexChar, Label->Font);
+            if(TotalString.Height < CharInfo.Char->Height) TotalString.Height = CharInfo.Char->Height;
+            TotalString.Width += CharInfo.Char->Width;
+        }
+        /* Tính lại vị trí Offset */
+        if(TotalString.Width < Label->Object.Size.Width) {
+            Offset.X += (Label->Object.Size.Width - TotalString.Width) / 2;
+        }
+        if(TotalString.Height < Label->Object.Size.Height) {
+            Offset.Y += (Label->Object.Size.Height - TotalString.Height) / 2;
+        }
+        
+        EZGUI_Draw_String(DrawPoint_Drv, Graphic, Offset, Label->Text, (void *)&Label->Color.ForeGround, Label->Font);
     }
 }
 
