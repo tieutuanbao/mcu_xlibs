808555c6420d18d42d7fe87b080e67ac0d505e84
tieutuanbao <tieutuanbao@gmail.com>
2022-11-16 16:55:02 +0700

    add Visible Image

--- components/EZGUI/source/ezgui_image.c
+++ components/EZGUI/source/ezgui_image.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 
 void Image_Draw(DrawPoint_t DrawPoint_Drv, EZGUI_Image_t *Image, Graphics_t *Graphic, Point_t Offset) {
-    Point_t DrawPos = {Image->Object.Point.X + Offset.X, Image->Object.Point.Y + Offset.Y};
-    Area_t ImageAreaCanDraw;
-    /* Ảnh nằm ngoài Graphic */
-    if(DrawPos.X > Graphic->Size.Width) return;     // Ảnh nằm ngoài bên phải Graphic
-    if(DrawPos.Y > Graphic->Size.Height) return;    // Ảnh nằm ngoài bên dưới Graphic
-    if((DrawPos.X + Image->Object.Size.Width) < 0) return;      // Ảnh nằm ngoài bên trái Graphic
-    if((DrawPos.Y + Image->Object.Size.Height) < 0) return;     // Ảnh nằm ngoài bên phải Graphic
+    if(Image->Object.Visible == true) {
+        Point_t DrawPos = {Image->Object.Point.X + Offset.X, Image->Object.Point.Y + Offset.Y};
+        Area_t ImageAreaCanDraw;
+        /* Ảnh nằm ngoài Graphic */
+        if(DrawPos.X > Graphic->Size.Width) return;     // Ảnh nằm ngoài bên phải Graphic
+        if(DrawPos.Y > Graphic->Size.Height) return;    // Ảnh nằm ngoài bên dưới Graphic
+        if((DrawPos.X + Image->Object.Size.Width) < 0) return;      // Ảnh nằm ngoài bên trái Graphic
+        if((DrawPos.Y + Image->Object.Size.Height) < 0) return;     // Ảnh nằm ngoài bên phải Graphic
 
-    if(DrawPos.X >= 0) ImageAreaCanDraw.Start.X = 0;
-    else ImageAreaCanDraw.Start.X = -DrawPos.X;
-    if(DrawPos.Y >= 0) ImageAreaCanDraw.Start.Y = 0;
-    else ImageAreaCanDraw.Start.Y = -DrawPos.Y;
+        if(DrawPos.X >= 0) ImageAreaCanDraw.Start.X = 0;
+        else ImageAreaCanDraw.Start.X = -DrawPos.X;
+        if(DrawPos.Y >= 0) ImageAreaCanDraw.Start.Y = 0;
+        else ImageAreaCanDraw.Start.Y = -DrawPos.Y;
 
-    ImageAreaCanDraw.Stop.X = (int32_t)Graphic->Size.Width - DrawPos.X - 1;
-    if(ImageAreaCanDraw.Stop.X > Image->Object.Size.Width) ImageAreaCanDraw.Stop.X = Image->Object.Size.Width - 1;
-    ImageAreaCanDraw.Stop.Y = (int32_t)Graphic->Size.Height - DrawPos.Y - 1;
-    if(ImageAreaCanDraw.Stop.Y > Image->Object.Size.Height) ImageAreaCanDraw.Stop.Y = Image->Object.Size.Height - 1;
-    
-    EZGUI_Draw_Image(DrawPoint_Drv, Graphic, DrawPos, Image->Object.Size, Image->Image_GetPixel);
+        ImageAreaCanDraw.Stop.X = (int32_t)Graphic->Size.Width - DrawPos.X - 1;
+        if(ImageAreaCanDraw.Stop.X > Image->Object.Size.Width) ImageAreaCanDraw.Stop.X = Image->Object.Size.Width - 1;
+        ImageAreaCanDraw.Stop.Y = (int32_t)Graphic->Size.Height - DrawPos.Y - 1;
+        if(ImageAreaCanDraw.Stop.Y > Image->Object.Size.Height) ImageAreaCanDraw.Stop.Y = Image->Object.Size.Height - 1;
+        
+        EZGUI_Draw_Image(DrawPoint_Drv, Graphic, DrawPos, Image->Object.Size, Image->Image_GetPixel);
+    }
 }
 
 
