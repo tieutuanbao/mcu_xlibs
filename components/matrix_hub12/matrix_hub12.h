d749fd765bad3ae1d4116d524f3b3fd35100023a
Tieu Tuan Bao <tieutuanbao@gmail.com>
2023-03-17 08:35:17 +0700

    add matrix_hub12 driver

---
+++ components/matrix_hub12/matrix_hub12.h
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file           : matrix_hub12.h
+  * @brief          : Thư viện điều khiển LED ma trận HUB12.
+  * @author         : Tiêu Tuấn Bảo
+  ******************************************************************************
+  * @attention
+  * 
+  */
+
+#ifndef __MATRIX_RGB__
+#define __MATRIX_RGB__
+
+typedef struct {
+    
+} MATRIX_HUB12_t;
+
+#endif
